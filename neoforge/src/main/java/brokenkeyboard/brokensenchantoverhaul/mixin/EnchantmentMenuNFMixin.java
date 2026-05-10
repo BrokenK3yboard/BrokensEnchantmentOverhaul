a4d1e0b72eddbbf9e0f62a2ee1ead78b61c296c4
BrokenK3yboard <63761658+BrokenK3yboard@users.noreply.github.com>
2026-05-09 19:09:25 -0500

    Add bookshelf enchanting bonus

--- neoforge/src/main/java/brokenkeyboard/brokensenchantoverhaul/mixin/EnchantmentMenuNFMixin.java
+++ neoforge/src/main/java/brokenkeyboard/brokensenchantoverhaul/mixin/EnchantmentMenuNFMixin.java
@@ -3,15 +3,20 @@ package brokenkeyboard.brokensenchantoverhaul.mixin;
 import brokenkeyboard.brokensenchantoverhaul.Config;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.IdMap;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.EnchantmentMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EnchantingTableBlock;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,6 +28,8 @@ import java.util.List;
 public class EnchantmentMenuNFMixin {
 
     @Shadow @Final public int[] levelClue;
+
+    @Shadow @Final private RandomSource random;
 
     // Modify the listed cost of an enchantment
     @WrapOperation(method = "lambda$slotsChanged$0", at = @At(value = "INVOKE",
@@ -38,5 +45,19 @@ public class EnchantmentMenuNFMixin {
         IdMap<Holder<Enchantment>> idmap = access.registryOrThrow(Registries.ENCHANTMENT).asHolderIdMap();
         Holder<Enchantment> slotValue = idmap.byId(menu.enchantClue[slot]);
         return Config.OVERHAUL_ENCHANTMENTS.get() && slotValue != null ? List.of(new EnchantmentInstance(slotValue, levelClue[slot])) : original.call(menu, access, stack, slot, cost);
+    }
+
+    @WrapOperation(method = "lambda$clickMenuButton$1", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;onEnchantmentPerformed(Lnet/minecraft/world/item/ItemStack;I)V"))
+    private void modifyExperienceCost(Player player, ItemStack stack, int levelCost, Operation<Void> original, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockPos blockPos) {
+        int bookshelves = 0;
+
+        for (BlockPos blockPos1 : EnchantingTableBlock.BOOKSHELF_OFFSETS) {
+            if (EnchantingTableBlock.isValidBookShelf(level, blockPos, blockPos1)) {
+                bookshelves++;
+            }
+        }
+
+        original.call(player, stack, random.nextDouble() < 0.01 * bookshelves ? 0 : levelCost);
     }
 }
